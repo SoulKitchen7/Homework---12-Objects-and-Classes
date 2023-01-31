0645558494d6e3ac40b598beff9bab0109c32ddd
Пользователь <whiskygogo@mail.ru>
2023-01-31 13:36:38 +0300

    Homework - 12 done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author erichRemarque = new Author("Эрих", "Ремарк");
+        System.out.println("Имя - " +erichRemarque.getName());
+        System.out.println("Фамилия - " +erichRemarque.getSurname());
+        Book threeLoads = new Book("Три товарища", 1936);
+        System.out.println("Название книги - " + threeLoads.getTitleOfTheBook()+ " Год издания - " + threeLoads.getYearOfPublication());
+
+        System.out.println("");
+
+        Author mikhailBulgakov = new Author("Михаил", "Булгаков");
+        System.out.println("Имя - " +mikhailBulgakov.getName());
+        System.out.println("Фамилия - " +mikhailBulgakov.getSurname());
+        Book theMasterAndMargarita = new Book("Мастер и Маргарита", 1967);
+        System.out.println("Название книги - " + theMasterAndMargarita.getTitleOfTheBook()+ " Год издания - " + theMasterAndMargarita.getYearOfPublication());
+        theMasterAndMargarita.setYearOfPublication(1940);
+        System.out.println("Название книги - " + theMasterAndMargarita.getTitleOfTheBook()+ " Год издания - " + theMasterAndMargarita.getYearOfPublication());
     }
 }
